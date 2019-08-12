bae18bff5d450df678ae241f5067b9a5fdf73edc
Rickson Thompson <ricksoncaique.tecgo@gmail.com>
2019-08-12 10:24:52 -0400

    Frontend finalizado

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -7,23 +7,49 @@ import Dropzone from 'react-dropzone';
 import logo from '../../assets/logo.svg';
 import './styles.css';
 import api from '../../services/api';
+import socket from 'socket.io-client';
 
 export default class Box extends Component {
   state = { box: {} }
 
   async componentDidMount() {
+    this.subscribeToNewFiles();
+
     const box = this.props.match.params.id;
+    console.log(this.props);
     //para pegar o id da box
     const response = await api.get(`boxes/${box}`);
-    //chamando a rota
-
+    //chamando a rota 
     this.setState({ box: response.data });
     //armazenando essas informações no state do componente
-}
+  } 
+
+subscribeToNewFiles = () => {
+  const box = this.props.match.params.id;
+  //pegando o id da box
+  const io = socket("https://week-6-b.herokuapp.com");
+  //conexão com o socket
+
+  io.emit("connectRoom", box);
+  //conectando a sala
+
+  io.on("file", data => {
+    this.setState({ box: { ... this.state.box, files: [data, ... this.state.box.files] } })
+    //inclui um novo state, e não altera o original. Segue o conceito de imutabilidade
+  });
+};
 
 handleUpload = (files) => {
   files.forEach(file => {
-    console.log(file);
+    const data = new FormData();
+    //enviar dados a api sem usar um form 
+    const box = this.props.match.params.id;
+    //id da box
+
+    data.append('file', file);
+    //adicionar uma informação, como na rota de file no campo 'file'
+
+    api.post(`boxes/${box}/files`, data);
   });
 }
 
